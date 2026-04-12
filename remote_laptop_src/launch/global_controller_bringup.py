bdeb8e3106685078a824017d7ff41c96bb870370
Yap Jia Wei <yaporx@gmail.com>
2026-04-12 20:05:38 +0800

    Merge upstream/jiawei and resolve cache file conflicts

--- remote_laptop_src/launch/global_controller_bringup.py
+++ remote_laptop_src/launch/global_controller_bringup.py
@@ -6,6 +6,7 @@ from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition
 import os
 
 
